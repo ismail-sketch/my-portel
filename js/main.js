b1ea3e329635a3ee91e7d442a4c7eca214f744bb
Gennadiy <gennadiy.mutalibov@gmail.com>
2020-11-10 13:36:37 +0300

    change_7

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 
 // let numberOfFilms;
 
@@ -61,14 +61,74 @@
 
 // function writeYourGeners() {
 //     for (let i = 1; i <= 3; i++) {
-//         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+//     personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
 //     }
 // } writeYourGeners();
 
-const options = {
-    name: 'run',
-    plase: 'full',
-    dream: 'bomb',
-    start: 'fluck'
-}
-console.log(Object.keys(options).length);
+
+//Урок 22 (основы js: Передача по ссылке или по значению
+
+let a = 5,
+    b = a;
+
+    b = b+5;
+
+    console.log(b);
+    console.log(a);
+
+    const obj = {
+        a: 5,
+        b: 1
+    };
+
+    //const copy = obj;// Здесь не делается копия obj, а передается именно ссылка на него
+
+    // copy.a = 10;  
+    
+    // console.log(copy);
+    // console.log(obj);
+
+    //Как получить копию объекта (испольование цикла):
+
+    function copy(mainObj) {
+        let objectCopy = {};
+
+        let key;
+        for (key in mainObj) {
+            objectCopy[key] = mainObj[key];
+        }
+        
+        return objectCopy;
+    }
+
+    const numbers = {
+        a: 2,
+        b: 5,
+        c: {
+            x: 7,
+            y: 4
+        }
+    };
+
+    const newNumbers = copy(numbers);
+
+    newNumbers.a = 10;
+
+    console.log(newNumbers);
+    console.log(numbers);
+
+
+
+
+
+
+
+//===============================================================================
+
+// const options = {
+//     name: 'run',
+//     plase: 'full',
+//     dream: 'bomb',
+//     start: 'fluck'
+// }
+// console.log(Object.keys(options).length);
