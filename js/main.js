5902cfecd820d4746189796ebdfa0a3c40260e81
Gennadiy <gennadiy.mutalibov@gmail.com>
2020-11-11 19:44:00 +0300

    change_10

--- js/main.js
+++ js/main.js
@@ -2,86 +2,41 @@
 
 
 
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    start: function () {
-        personalMovieDB.count = +prompt ('Сколько фильмов вы уже просмотрели?', '');
-    
-        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt ('Сколько фильмов вы уже просмотрели?', '');
-        }
-    },
-    rememberMyFilms: function () {
-        for (let i = 0; i < 2; i++) {
-            const a = prompt('Один из последних просмотренных фильмов', ''),
-                  b = prompt('На сколько оцените его?', '');
-                  
-            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-                personalMovieDB.movies[a] = b;
-                console.log('done');
-            
-            }else {
-                i--;
-                console.log('eror');
-            } 
-        }
-    },
-    detectPersonalLevel: function () {
-        if (personalMovieDB.count < 10) {
-            console.log('Просмотрено мало фильмов');
-        }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            console.log('Вы классический зритель');     
-        }else if (personalMovieDB.count >= 30) {
-            console.log('Вы киноман');
-        }else {
-            console.log('Произошла обшика!');
-        }
-    },
-    showMyDB: function (hidden) {
-        if (!hidden) {
-            console.log (personalMovieDB);
-        }
-    },
-    tooggleVisibleMyDB: function() {
-        if (personalMovieDB.privat) {
-            personalMovieDB.privat = false;
-        }else {
-            personalMovieDB.privat = true;
-        } 
-    },
-    writeYourGeners: function() {
-        for (let i = 1; i <= 3; i++) {
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
-            
-            if (genre === '' || genre === null) {
-                console.log('Вы ввели некорректные данные или не ввели их вовсе!');
-                i--;
-            }else {
-                personalMovieDB.genres[i - 1] = genre;
-            }
-        }
-    
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`Любимый жанр ${i+1} - это ${item}`);
-        });
-    }
-}; 
+const titleHero = document.querySelector('.title');
+let btns = document.querySelectorAll('button'); //Псевдомассив 
+const wrapperDiv = document.querySelector('.wrapper')
+
+console.log(btns);
+
+// titleHero.style.backgroundColor = 'blue';
+
+btns[1].style.borderRadius = '100%';
+
+titleHero.style.cssText = 'background-color: blue; width: 700px; margin: 0 auto';
+
+// for (let i = 0; i < btns.length; i++) {
+//     btns[i].style.backgroundColor = 'red';
+// }
 
 
+//Работа со всеми элементами полученного псевдомассива:
+btns.forEach(item => {
+    item.style.backgroundColor = 'red';
+});
 
+//Основные методы для работы с элементами страницы:
 
+const div = document.createElement('div'); //Создание элемента (в данном случае div) 
+//const text = document.createTextNode('Сюда помещается текст, который необходим');  //Создание текстового узла (элемента без оболочки тега)
 
+div.classList.add('title'); //Свойство classList в котором мы через точку можем писать действия с нашим классом
+//document.body.append(div);//Добавление div, созданного в JS на страницу в конец body 
 
+//wrapperDiv.append(div);//Добавление div, созданного в JS, на страницу в КОНЦЕ какого-либо элемента
+wrapperDiv.prepend(div); //Добавление div, созданного, в JS на страницу в НАЧАЛЕ какого-либо элемента
 
+//btns[1].remove(); //Удаление элемента
 
-
-
-
-
-
-
-
+//Метод replaceWith используется для замены одного элемента на другой
+div.innerHTML = '<h2>Привет, Геша</h2>'; //Добавление текста в элемент и вообще HTML элементы 
+div.insertAdjacentHTML('afterbegin', '<h2>Hello</h2>'); //Вставление HTML кусков в элементы
