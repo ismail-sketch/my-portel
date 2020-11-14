17f3818afab0b87c8d549f562655e6aa21992497
Gennadiy <gennadiy.mutalibov@gmail.com>
2020-11-14 15:10:30 +0300

    Урок 37 classList и делегирование событий

--- js/main.js
+++ js/main.js
@@ -2,65 +2,32 @@
 
 
 
-const titleHero = document.querySelector('.title');
-let btns = document.querySelectorAll('button'); //Псевдомассив 
-const wrapperDiv = document.querySelector('.wrapper')
 
-console.log(btns);
-
-// titleHero.style.backgroundColor = 'blue';
-
-btns[1].style.borderRadius = '100%';
-
-titleHero.style.cssText = 'background-color: blue; width: 700px; margin: 0 auto';
-
-// for (let i = 0; i < btns.length; i++) {
-//     btns[i].style.backgroundColor = 'red';
-// }
+const btns = document.querySelectorAll('button'), //Псевдомассив 
+	  wrp = document.querySelector('.title');
 
 
-//Работа со всеми элементами полученного псевдомассива:
-btns.forEach(item => {
-    item.style.backgroundColor = 'red';
-});
+ 
+ // Урок 37 classList и делегирование событий
 
-//Основные методы для работы с элементами страницы:
-
-const div = document.createElement('div'); //Создание элемента (в данном случае div) 
-//const text = document.createTextNode('Сюда помещается текст, который необходим');  //Создание текстового узла (элемента без оболочки тега)
-
-div.classList.add('title'); //Свойство classList в котором мы через точку можем писать действия с нашим классом
-//document.body.append(div);//Добавление div, созданного в JS на страницу в конец body 
-
-//wrapperDiv.append(div);//Добавление div, созданного в JS, на страницу в КОНЦЕ какого-либо элемента
-wrapperDiv.prepend(div); //Добавление div, созданного, в JS на страницу в НАЧАЛЕ какого-либо элемента
-
-//btns[1].remove(); //Удаление элемента
-
-//Метод replaceWith используется для замены одного элемента на другой
-div.innerHTML = '<h2>Привет, Геша</h2>'; //Добавление текста в элемент и вообще HTML элементы/Добавить ТЕКСТ еще можно с помощью textContent 
-div.insertAdjacentHTML('afterbegin', '<h2>Hello</h2>'); //Вставление HTML кусков в элементы
-
-// =====================================================================================
-
-// СОБЫТИЯ И ИХ ОБРАБОТЧИКИ 
-
-const deletElement = (e) => {
-	console.log(e.target);
-	console.log(e.type);
-
-	/* i++
-	 if (i == 1) {
-	 	btn.removeEventListener('click', deletElement);
+ 
+ btns[0].addEventListener('click', () => {
+	// if (!btns[1].classList.contains('red')) {
+	// 	btns[1].classList.add('red');
+	// }else {
+	// 	btns[1].classList.remove('red');
+	// }
+	btns[1].classList.toggle('red');
+ });
+ 
+ wrp.addEventListener('click', (e) => {
+	if (e.target && e.target.tagName == 'BUTTON') {
+		console.log('Hello');
 	}
-};*/
 
-btn.addEventListener('click', deletElement, {once: true}); // {once: true} - означает, что клик произойдет только один раз, после чего уже работать не будет
-overlay.addEventListener('click', deletElement);  /*Элементов btn и overlay у меня здесь нет, 
- поэтому они не сработают. Это просто для примера. */
+ });
 
- // Отменить стандартное поведение браузера:
+ const btn = document.createElement('button');
+ btn.classList.add('ellow');
+ wrp.append(btn);
 
- link.addEventListener('click' (e) => {
- 	e.preventDefault();
- });// Этот код всегда помещается в самое начало обработчика событий. 
