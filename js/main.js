b4c1af0d8171e7892141def196b040fe8e454bbe
Gennadiy <gennadiy.mutalibov@gmail.com>
2020-11-15 19:36:43 +0300

    Параметры документа, окна и работа с ними. Урок 42

--- js/main.js
+++ js/main.js
@@ -1,65 +1,19 @@
 "use strict";
 
-// setTimeout и setInterval 39 урок 
+// Параметры документа, окна и работа с ними
 
-let timerId,
-	i = 0;
-	
-	function myAnimation() {
-		const elem = document.querySelector('.square');
-		let pos = 0;
+const wrp = document.querySelector('.wrp'),
+      btn = document.querySelector('.btn');
 
-		const id = setInterval(frame, 10);
-		function frame() {
-			if (pos == 400) {
-				clearInterval(id);
-			}else {
-				pos++;
-				elem.style.top = pos + 'px';
-				elem.style.left = pos + 'px';
-			}
-		} 
-	}
+const width = wrp.clientWidth;
+const height = wrp.clientHeight;
 
-const btn = document.querySelector('.button_1');
+console.log(width, height);
 
-btn.addEventListener('click', myAnimation);
+btn.addEventListener('click', () => {
+    //wrp.style.height = wrp.scrollHeight + 'px';
+    console.log(wrp.scrollTop);
+});
 
- 
-
- 
-
-// function logger () {
-// 	if (i === 3) {
-// 		clearInterval(timerId);
-// 	}
-// 	console.log('text');
-// 	i++;
-// }
-
-// let id = setTimeout(function log(){
-// 	console.log('Hello');
-// 	id = setTimeout(log, 500);
-// }, 500);
-
-// РАБОТА С ДАТАМИ УРОК 40
-
-const now = new Date();
-
-//Получение дат
-
-// console.log(now.getFullYear());
-// console.log(now.getMonth());
- //console.log(now.getDate(1605434429845));
-// console.log(now.getHours());
-// console.log(now.getUTCHours());
-//console.log(now.getTime());
-
-// Методы установки дат
-
-console.log(now.setHours(18));
-console.log(now);
-
-
-
+console.log(wrp.getBoundingClientRect().top);
 
