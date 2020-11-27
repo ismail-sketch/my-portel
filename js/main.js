d08b338a37af1f46d026a443234f97392926e494
Gennadiy <gennadiy.mutalibov@gmail.com>
2020-11-27 19:45:15 +0300

    Методы перебора массивов

--- js/main.js
+++ js/main.js
@@ -1,123 +1,34 @@
 "use strict";
+/* jslint node: true */
+/* jshint browser: true */
+/*jshint esversion: 6 */
 
-// КОНТЕКСТ ВЫЗОВА ФУНКЦИЙ УРОК 46
+// REST ОПЕРАТОР И ПАРМЕТРЫ ПО УМОЛЧАНИЮ (ES6) УРОК 49
 
-// function showThis(a, b) {
-//     console.log(this);
-//     function sum() {
-//         console.log(this);
-//         return a + b;
-//     }
+const log = function(a, b, ...rest) {
+    console.log(a, b, rest);
+};
 
-//     console.log(sum());
-// }
-// showThis(4, 5);
+log('basic', 'plump', 'operator', 'usage');
 
-// const obj = {
-//     a: 20,
-//     b: 15,
-//     dum: function() {
-//         function shout() {
-//             console.log(this);
-//         }
-//         shout();
-//     }
-// };
-
-// obj.dum();
-
-// function User(name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-// }
-//     let ivan = new User('Ivan', 28);
-
-// function sayName(surname) {
-//     console.log(this);
-//     console.log(this.name + surname);
-// }
-
-// const user = {
-//     name: 'John'
-// };
-
-// sayName.call(user, 'Smith'); // Метод присвоение контекста для функции
-// sayName.apply(user, ['Smith']); // Метод присвоение контекста для функции
-
-// function count(num) {
-//     return this*num;
-// }
-
-// const double = count.bind(2);
-// console.log(double(3));
-// console.log(double(13));
-
-
-// 1) Обычная функция: this = window, но если стоит use strict, то будет undefined 
-// 2) Констекстом у методов объекта будет сам объект
-// 3) this в конструкторах и классах - это новый экземпляр объекта
-// 4) Ручная привязка this: call, apply, bind
-// 5) У стрелочной функции нет своего контекста, и она ссылается на родителя
-
-// const btn = document.querySelector('button');
-
-// btn.addEventListener('click', function(){
-//    this.style.backgroundColor = 'green';
-// });
-
-// const obj = {
-//     num: 5,
-//     sayNumber: function() {
-//         const say = () => {
-//             console.log(this);
-//         };
-
-//         say();
-//     }
-// };
-
-// obj.sayNumber();
-
-// const double = a => a * 2; // Если тело стрелочной функции помещается в одну строку, то она может быть записана таким образом
-
-// console.log(double(4));
-
-// =============================================================================
-
-// КЛАССЫ ES6 47 УРОК 
-
-class Rectangle {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
-    }
-
-    calcAreal() {
-        return this.height * this.width;
-    }
+function calcOrDouble(number, basis = 2) {
+    console.log(number * basis);
 }
 
-class ColoredRectangleWithText extends Rectangle {
-    constructor(height, width, text, bgColor) {
-        super(height, width);
-        this.text  = text;
-        this.bgColor = bgColor;
-    }
+calcOrDouble(3);
 
-    showMyProps() {
-        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
-    }
-}
+// Методы перебора массивов
 
-const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
+const obj = {
+    ivan: 'persone',
+    ann: 'persone',
+    dog: 'animal',
+    cat: 'animal'
+};
 
-div.showMyProps();
-console.log(div.calcAreal());
-
-
-// const square = new Rectangle(10, 10);
-// const long = new Rectangle(20, 100);
-
-// console.log(square.calcAreal());
-// console.log(long.calcAreal());
+const newArr = Object.entries(obj)// Метод, делающий из объекта массив
+.filter((item) => {
+   return item[1] === 'persone'; // Можно либо так функцию записывать, либо так как ниже, если она в одну строку
+})
+.map(item => item[0]);
+console.log(newArr);
